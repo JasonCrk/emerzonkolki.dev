a0f5ff6d34d29f6fd11e8f3b9916af6f2ff45fa1
JasonCrk <emerzonkolki@gmail.com>
2024-01-28 22:29:55 -0500

    feat(Link.tsx): add extraClasses prop

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -8,17 +8,24 @@ type Props = {
   children: string
   href?: string
   isExternalLink?: boolean
+  extraClasses?: string
 }
 
-const Link: FC<Props> = ({ children, isExternalLink, href, Icon }) => {
+const Link: FC<Props> = ({
+  children,
+  isExternalLink,
+  href,
+  extraClasses,
+  Icon
+}) => {
   return (
     <a
       href={href}
-      className={`flex items-center justify-center justify-items-center bg-dark-400/5 dark:bg-dark-800 rounded-full overflow-hidden py-[1.5px] px-[1.49px] relative z-10 ${!href ? 'cursor-default' : 'cursor-pointer'}`}
+      className={`w-fit bg-dark-400/5 dark:bg-dark-800 rounded-full overflow-hidden relative z-10 ${!href ? 'cursor-default' : 'cursor-pointer'}`}
       target={isExternalLink ? '_blank' : undefined}
     >
       <div
-        className={`flex items-center justify-center text-sm bg-grey-50 text-dark-400 dark:text-white px-[6px] py-[5px] rounded-full gap-x-1 dark:bg-dark-800 before:absolute before:-bottom-2 before:-left-2 before:w-full before:h-9 before:rounded-3xl before:bg-grey-200 before:-z-10 before:content-[''] before:blur-sm hover:before:h-20 hover:before:w-[115%] before:transition-all before:duration-[300ms]`}
+        className={`flex items-center justify-center text-sm bg-grey-50 text-dark-400 dark:text-white px-2 py-1.5 m-[0.15rem] rounded-full gap-x-1 dark:bg-dark-800 before:absolute before:-bottom-2 before:-left-2 before:w-full before:h-9 before:rounded-3xl before:bg-grey-200 before:-z-10 before:content-[''] before:blur-sm hover:before:h-20 hover:before:w-[115%] before:transition-all before:duration-[300ms] ${extraClasses}`}
       >
         {Icon && <Icon className='text-xl' />}
 
