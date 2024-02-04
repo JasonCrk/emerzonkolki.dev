075880296e8fd8e4e01f479b046a8e2854dd3522
JasonCrk <emerzonkolki@gmail.com>
2024-02-03 21:51:12 -0500

    style(Link.tsx): change margin

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -25,7 +25,7 @@ const Link: FC<Props> = ({
       target={isExternalLink ? '_blank' : undefined}
     >
       <div
-        className={`flex items-center justify-center text-sm bg-grey-50 text-dark-400 dark:text-white px-2 py-1.5 m-[0.15rem] rounded-full gap-x-1 dark:bg-dark-800 before:absolute before:-bottom-2 before:-left-2 before:w-full before:h-9 before:rounded-3xl before:bg-grey-200 before:-z-10 before:content-[''] before:blur-sm hover:before:h-20 hover:before:w-[115%] before:transition-all before:duration-[300ms] ${extraClasses}`}
+        className={`flex items-center justify-center text-sm bg-grey-50 text-dark-400 dark:text-white px-2 py-1.5 m-[1.6px] rounded-full gap-x-1 dark:bg-dark-800 before:absolute before:-bottom-2 before:-left-2 before:w-full before:h-9 before:rounded-3xl before:bg-grey-200 before:-z-10 before:content-[''] before:blur-sm hover:before:h-20 hover:before:w-[115%] before:transition-all before:duration-[300ms] ${extraClasses}`}
       >
         {Icon && <Icon className='text-xl' />}
 
