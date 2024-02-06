e6c5e84a85223b62dd4726fd49c55830c34a106c
JasonCrk <emerzonkolki@gmail.com>
2024-02-05 23:33:46 -0500

    feat: contrast color

--- src/types/skills.ts
+++ src/types/skills.ts
@@ -20,7 +20,7 @@ export type Skill =
 
 export const skillTagStyleClasses = {
   javascript: 'bg-[#f7e0251a] border-[#f7e025] text-[#f7e025]',
-  typescript: 'bg-[#377cc81a] border-[#377cc8] text-[#377cc8]',
+  typescript: 'bg-[#1A7BE61a] border-[#1A7BE6] text-[#1A7BE6]',
   react: 'bg-[#00d8ff1a] border-[#00d8ff] text-[#00d8ff]',
   angular: 'bg-[#f50b481a] border-[#f50b48] text-[#f50b48]',
   tailwindcss: 'bg-[#38bdf81a] border-[#38bdf8] text-[#38bdf8]',
@@ -28,13 +28,13 @@ export const skillTagStyleClasses = {
   bootstrap: 'bg-[#800af91a] border-[#800af9] text-[#800af9]',
   nodejs: 'bg-[#23b45d1a] border-[#23b45d] text-[#23b45d]',
   expressjs:
-    'dark:bg-[#ffffff1a] dark:border-white dark:text-white bg-[#0000001a] border-dark-900 text-dark-900',
-  java: 'bg-[#0175bd1a] border-[#0175bd] text-[#0175bd]',
+    'dark:bg-[#ffffff1a] dark:border-white dark:text-white bg-dark-400/10 border-dark-800 text-dark-800',
+  java: 'bg-[#0094F01a] border-[#0094F0] text-[#0094F0]',
   'spring boot': 'bg-[#6db33f1a] border-[#6db33f] text-[#6db33f]',
-  python: 'bg-[#3979ab1a] border-[#3979ab] text-[#3979ab]',
+  python: 'bg-[#388ED01a] border-[#388ED0] text-[#388ED0]',
   django: 'bg-[#27ab781a] border-[#27ab78] text-[#27ab78]',
-  mysql: 'bg-[#00678f1a] border-[#00678f] text-[#00678f]',
-  postgresql: 'bg-[#3367911a] border-[#336791] text-[#336791]',
+  mysql: 'bg-[#0E9AD01a] border-[#0E9AD0] text-[#0E9AD0]',
+  postgresql: 'bg-[#4595D61a] border-[#4595D6] text-[#4595D6]',
   mongodb: 'bg-[1a] border-[] text-[]',
   docker: 'bg-[#2497ed1a] border-[#2497ed] text-[#2497ed]',
   kubernetes: 'bg-[#2e6ce61a] border-[#2e6ce6] text-[#2e6ce6]'
